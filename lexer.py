e53efc422b0b28dbce4537724050f1ce8d02ca04
PortalGd <tykenjcruz@gmail.com>
2026-05-06 17:00:03 -0700

    Lexer.py update
    WHY ISNT IT WORKING I EVEN ADDED TESTS AGGGGGGRIOGBKJBDSFOBLAMSDNCLIBADUOSFSAHOUFC

--- lexer.py
+++ lexer.py
@@ -68,7 +68,7 @@ class Lexer:
         self.pos = 0
         self.line = 1
         self.col = 1
-        self.tokens: List[Token] = []
+        self.tokens: list[Token] = []
 
         self.keywords = {
             'let': TokenType.LET,
@@ -80,32 +80,207 @@ class Lexer:
             'false': TokenType.FALSE
         }
 
-def current_char(self):
-    if self.pos >= len(self.code):
-        return None
-    return self.code[self.pos]
+    def current_char(self):
+        if self.pos >= len(self.code):
+            return None
+        return self.code[self.pos]
 
-def peek(self, offset: int = 1):
-    pos = self.pos + offset
-    if pos >= len(self.code):
-        return None 
-    return self.code[pos]
+    def peek(self, offset: int = 1):
+        pos = self.pos + offset
+        if pos >= len(self.code):
+            return None 
+        return self.code[pos]
 
-def advance(self, steps: int = 1):
-    if self.pos < len(self.code):
-        if self.code[self.pos] == '\n':
-            self.line += 1
-            self.col = 1
-        else:
-            self.col += 1
+    def advance(self, steps: int = 1):
+        if self.pos < len(self.code):
+            if self.code[self.pos] == '\n':
+                self.line += 1
+                self.col = 1
+            else:
+                self.col += 1
 
-def skip_whitespace(self):
-    while self.current_char() and self.current_char() in '\t':
+    def skip_whitespace(self):
+        while self.current_char() and self.current_char() in '\t':
+            self.advance()
+
+    def skip_comment(self):
+        if self.current_char() == '/' and self.peek() == '/':
+            while self.current_char() and self.current_char() != '\n':
+                self.advance()
+
+    def read_string(self, quote_char: str) -> str:
+        value = ''
         self.advance()
 
-def skip_comment(self):
-    if self.current_char() == '/' and self.peek() == '/':
-        while self.current_char() and self.current_char() != '\n':
+        while self.current_char() and self.current_char() != quote_char:
+            if self.current_char() == '\\': 
+                self.advance()
+                if self.current_char() == 'n':
+                    value += '\n'
+                elif self.current_char() == 't':
+                    value += '\t'
+                elif self.current_char() == '\\':
+                    value += '\\'
+                else:
+                    value += self.current_char()
+                self.advance()
+            else:
+                value += self.current_char()
+                self.advance()
+        
+        if self.current_char() == quote_char:
+            self.advance() 
+        return value
+
+    def read_number(self) -> Token:
+        sl = self.line
+        sc = self.col
+        num_str = ''
+
+        while self.current_char() and (self.current_char().isdigit() or self.current_char() == "."):
+            num_str += self.current_char()
             self.advance()
+        token_type = TokenType.FLOAT if '.' in num_str else TokenType.INT
+        return Token(token_type, num_str, sl, sc)
+    
+    def read_identifier(self) -> Token:
+        sl = self.line
+        sc = self.col
+        id_str = ''
+
+        while self.current_char() and (self.current_char().isalnum() or self.current_char() == '_'):
+            id_str += self.current_char()
+            self.advance()
+        
+        token_type = self.keywords.get(id_str, TokenType.IDENTIFIER)
+        return Token(token_type, id_str, sl, sc)
+    
+    def tokenize(self) -> list[Token]:
+        while self.pos < len(self.code):
+            self.skip_whitespace()
+
+            if self.pos >= len(self.code):
+                break
+
+            if self.current_char() == '/' and self.peek() == '/':
+                self.skip_comment()
+                continue
+
+            sl = self.line
+            sc = self.col
+            char = self.current_char()
+
+            if char == "\n":
+                self.advance()
+                continue
+
+            if char in ('"', "'"):
+                self.tokens.append(Token(TokenType.STRING, self.read_string(char), sl, sc))
+                continue
+
+            if char.isdigit():
+                self.tokens.append(self.read_number())
+                continue
+
+            if char.isalpha() or char == '_':
+                self.tokens.append(self.read_identifier())
+                continue
+
+            if char == "!" and self.peek() == "=":
+                self.tokens.append(Token(TokenType.NOT_EQUAL, '!=', sl, sc))
+                self.advance(2)
+                continue
+
+            if char == "<" and self.peek() == "=":
+                self.tokens.append(Token(TokenType.LESS_EQUAL, '<=', sl, sc))
+                self.advance(2)
+                continue
+
+            if char == ">" and self.peek() == "=":
+                self.tokens.append(Token(TokenType.GREATER_EQUAL, '>=', sl, sc))
+                self.advance(2)
+                continue
+
+            if char == "&" and self.peek() == "&":
+                self.tokens.append(Token(TokenType.AND_AND, '&&', sl, sc))
+                self.advance(2)
+                continue
+
+            if char == "|" and self.peek() == "|":
+                self.tokens.append(Token(TokenType.OR_OR, '||', sl, sc))
+                self.advance(2)
+                continue
+
+            if char == '-' and self.peek() == '>':
+                self.tokens.append(Token(TokenType.ARROW, '->', sl, sc))
+                self.advance(2)
+                continue
+
+            single_char_tokens = {
+                '+': TokenType.PLUS,
+                '-': TokenType.MINUS,
+                '*': TokenType.STAR,
+                '/': TokenType.SLASH,
+                '%': TokenType.PERCENT,
+                '=': TokenType.EQUAL,
+                '<': TokenType.LT,
+                '>': TokenType.GT,
+                '!': TokenType.BANG,
+                '(': TokenType.LPAREN,
+                ')': TokenType.RPAREN,
+                '{': TokenType.LBRACE,
+                '}': TokenType.RBRACE,
+                '[': TokenType.LBRACKET,
+                ']': TokenType.RBRACKET,
+                ';': TokenType.SEMICOLON,
+                ',': TokenType.COMMA,
+                ':': TokenType.COLON,
+            }
+
+            if char in single_char_tokens:
+                self.tokens.append(Token(single_char_tokens[char], char, sl, sc))
+                self.advance()
+                continue
+
+            raise SyntaxError(f"Unexpected character '{char}' at line {sl}:{sc}")
+        
+        self.tokens.append(Token(TokenType.EOF, '', self.line, self.col))
+        return self.tokens
+
+def tokenize(code: str) -> list[Token]:
+    lexer = Lexer(code)
+    return lexer.tokenize()
+
+if __name__ == "__main__":
+    # Test 1: Simple let binding
+    code1 = "let x = 5;"
+    print("Test 1:", code1)
+    tokens1 = tokenize(code1)
+    for token in tokens1:
+        print(f"  {token}")
+    print()
+    
+    # Test 2: Expression
+    code2 = "x + 3 * 2"
+    print("Test 2:", code2)
+    tokens2 = tokenize(code2)
+    for token in tokens2:
+        print(f"  {token}")
+    print()
+    
+    # Test 3: Function definition
+    code3 = "fn add(a: int, b: int) -> int { a + b }"
+    print("Test 3:", code3)
+    tokens3 = tokenize(code3)
+    for token in tokens3:
+        print(f"  {token}")
+    print()
+    
+    # Test 4: String literal
+    code4 = 'let msg = "hello world";'
+    print("Test 4:", code4)
+    tokens4 = tokenize(code4)
+    for token in tokens4:
+        print(f"  {token}")
 
 
